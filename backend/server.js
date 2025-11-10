852952890f99747040854fc1f58e7324d047dc47
gonzalodom11 <jgdominguezmoreno@gmail.com>
2025-11-10 12:30:43 +0100

    feat: user model

--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,5 @@ app.use('/api/sensor', sensorRoutes);
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
