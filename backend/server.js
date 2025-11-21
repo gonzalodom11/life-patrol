4d75549da9b148f818162a75c96cf0efcf2c53a5
gonzalodom11 <jgdominguezmoreno@gmail.com>
2025-11-21 01:38:39 +0100

    fix: cors in production

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,17 @@ const app = express();
 // Middleware
 const allowedOrigin = process.env.ALLOWED_ORIGIN
 
-app.use(cors({
-  origin: allowedOrigin,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 // Connect to MongoDB Atlas
 connectDB();
 
